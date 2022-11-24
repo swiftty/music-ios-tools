b408de3f72aa98b3c8eab1577fc6de99788f9f1b
swiftty <62803132+swiftty@users.noreply.github.com>
2022-11-24 16:10:10 +0900

    add swift package

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "cli",
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/yonaskolb/XcodeGen", from: "2.0.0"),
+        .package(url: "https://github.com/realm/SwiftLint", from: "0.43.0"),
+        .package(url: "https://github.com/swiftty/LicenseGen", from: "0.0.7")
+    ]
+)
